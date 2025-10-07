1d50540567d28ba0857c2ee8f01bbf2022d9b5e2
Diego Ramirez <diego.ramirez2d03@gmail.com>
2025-10-07 11:30:53 -0600

    feat: GET method to recover all car records

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const cars = require('./cars.json')
 
 const app = express();
+app.disable('x-powered-by')
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello expres' })
+app.get('/cars', (req, res) => {
+  res.json(cars)
 })
+
+
 
 const PORT = process.env.PORT ?? 3001;
 app.listen(PORT, () => {
